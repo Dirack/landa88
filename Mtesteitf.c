32a2236e483434e83d3bd45b7d4ec829a3be29dd
Dirack <rodolfo_profissional@hotmail.com>
2021-12-23 22:29:44 -0300

    [WIP] Create an ADT for interfaces #4
    Create an Abstract Data Type for model interfaces

---
+++ Mtesteitf.c
@@ -0,0 +1,17 @@
+#include <rsf.h>
+#include "interface2d.h"
+
+int main(int argc, char* argv[]){
+	
+	itf2d itf;
+	float *sz;
+	int i;
+
+	sz = sf_floatalloc(11);
+
+	for(i=0;i<11;i++)
+		sz[i]=1.0;
+	
+	itf = itf2d_init(sz,11,-2.0,1.0);
+
+}
---
+++ interface2d.c
@@ -0,0 +1,48 @@
+#include <rsf.h>
+#include "interface2d.h"
+#include "cubicsplineint.h"
+
+#ifndef _interface2d_h_
+
+typedef struct Interface2d *itf2d;
+/* abstract data type */
+/*^*/
+
+#endif
+
+struct Interface2d{
+	float *coef;
+	int n;
+	float o;
+	float d;
+};
+/* Concrete data type */
+
+itf2d itf2d_init(float *sz,
+		 int n1,
+		 float o1,
+		 float d1)
+/*< TODO >*/
+{
+	itf2d itf;
+	int i;
+	float *x;
+
+	itf = (itf2d) sf_alloc(1,sizeof(struct Interface2d));
+
+	itf->n = n1;
+	itf->o = o1;
+	itf->d = d1;
+
+	x = sf_floatalloc(n1);
+
+	for(i=0;i<n1;i++){
+		x[i] = i*d1+o1;
+	}
+
+	/* Calculate coefficients matrix (interfaces interpolation) */
+	itf->coef = sf_floatalloc(4*(n1-1));
+	calculateSplineCoeficients(n1,x,sz,&itf->coef,1);
+
+	return itf;
+}
