b0c3085ec5e892bc5ebc50f299ebdac2a7cb5f58
Dirack <rodolfo_profissional@hotmail.com>
2022-02-16 12:16:16 -0300

    Scratch of model2d and layer2d unit tests #8

---
+++ test/TDD/test_layer2d.c
@@ -0,0 +1,31 @@
+/*
+* test_layer2d.c (C)
+* 
+* Purpose: Unit tests of layer2d struct.
+* 
+* Site: https://dirack.github.io
+* 
+* Version 1.0
+* 
+* Programmer: Rodolfo A C Neves 16/02/2022
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* License: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+#include "Unity/unity.h"
+#include "interface2d.h"
+#include "layer2d.h"
+#include <stdio.h>
+#include <rsf.h>
+
+void setUp(){};
+
+void tearDown(){};
+
+int main(void){
+
+	UNITY_BEGIN();
+	return UNITY_END();
+}
